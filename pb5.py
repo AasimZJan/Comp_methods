bc47de53e634ea207c51ef972b6c7b9d45ecdf73
AasimZahoor <zahoorassim@gmail.com>
2020-09-02 17:03:16 -0400

    Generalised the interpolator and made necessary changes in pb6

--- pb5.py
+++ pb5.py
@@ -8,12 +8,15 @@ Created on Tue Sep  1 11:00:53 2020
 
 #%%
 #interpolator
-def interpolator(x0,y0,x1,y1):
-    m=(y1-y0)/(x1-x0)             #calculationg slope since it is linear interpolation 
-    def f(x):
-        return(m*(x-x0)+y0)
-    return(f)
+def interpolator(x,y,a):
+    #calculationg slope since it is linear interpolation
+    yp=[]
+    for i in range(len(x)-1):
+        m=(y[i+1]-y[i])/(x[i+1]-x[i])              
+        yp.append(m*(a[i]-x[i])+y[i])
+    #When you call this function you get a function when inputted a point gives you the interpolated y values
+    return(yp)                     
     
-    
+
     
     
--- pb6.py
+++ pb6.py
@@ -8,22 +8,28 @@ Created on Tue Sep  1 11:01:28 2020
 
 #%%
 #problem 6
-from pb5 import interpolator
-x=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-y=[0.0,0.0,0.001,0.008,0.030,0.065,0.103,0.130,0.140,0.132,0.113,0.089,0.066,0.0460,.030,0.019,0.012,0.007,0.004,0.002,0.001]
-xp=[0.5,1.5,2.5,3.5,4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5,19.5]
-yp=[]
-for i in range(len(xp)):
-    l=interpolator(x[i+1],y[i+1],x[i],y[i])
-    yp.append(l(xp[i]))
+import matplotlib.pyplot as plt
+import numpy as np
+#importing interpolator from problem 5
+from pb5 import interpolator     
+#extracting data
+data = np.loadtxt('lens_density.txt',delimiter=',',skiprows=1)   #make sure to check the file path
+x=data[:,0]
+y=data[:,1]
+#generating the points we are interested in
+xp=[]
+for i in range(len(x)-1):
+    xp.append(0.5*(x[i]+x[i+1]))
+#calling the interpolator
+yp=interpolator(x,y,xp)
 print(yp)
+#plotting
+plt.xlabel("interested x values")
+plt.ylabel("Corresponding y valuesls")
+plt.title("Interpolated points")
+plt.plot(xp,yp,marker='o')
+plt.savefig("HW_1_pb6.png")
 
-    
-#%%
-#trying out interpolator
-from pb5 import interpolator
-y=interpolator(0,5,1,10)
-x=[1,2,0.5]
 
-    
+
     
