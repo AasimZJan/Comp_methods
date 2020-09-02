ae4b4dd4ef64c54351c263eb2efe38d921f2a1f5
AasimZahoor <zahoorassim@gmail.com>
2020-09-02 18:07:57 -0400

    added plots, allowed the scripts to take functions and debugged newton's

--- pb1.py
+++ pb1.py
@@ -36,3 +36,40 @@ def bisection(a,b,f,t=0.000001):
     return(x2,i)
  #   print('the number of iterations is', i)
  #   print("the value of x is",x2)
+def secant(a,b,f,t=0.000001):
+    x0=a
+    x1=b
+    y0=f(x0)
+    y1=f(x1)
+    x2=x1-(float(x1-x0)/(y1-y0))*y1
+    y2=f(x2)
+    i=1
+    while ((x2-x1)/x1)>t or ((x2-x1)/x1)< -t:
+        if y2*y1<0:
+            x0=x1
+            x1=x2
+            y0=f(x0)
+            y1=f(x1)
+            x2=x1-(float(x1-x0)/(y1-y0))*y1
+            y2=f(x2)
+            i=i+1
+        else:
+            x0=x0
+            x1=x2
+            y0=f(x0)
+            y1=f(x1)
+            x2=x1-(float(x1-x0)/(y1-y0))*y1
+            y2=f(x2)
+            i=i+1
+    return(x2,i)
+#    print("the number of iterations is ",i)
+#    print("the value of x is",x2)
+def newton(a,f,df,t=0.000001):
+    x0=a
+    x1=x0-(f(x0)/df(x0))
+    i=1
+    while ((x1-x0)/x0)>t or ((x1-x0)/x0)< -t:
+        x0=x1
+        x1=x0-(f(x0)/df(x0))
+        i=i+1
+    return(x1,i)
--- pb2.py
+++ pb2.py
@@ -8,14 +8,41 @@ Created on Tue Sep  1 08:27:59 2020
 
 #%%
 #problem 2
-import rf
-def h(x):
-    return((0.5-((1+x**2)**(-0.5)))/(x*((1+x**2)**1.5)))
-def f(x):   #define yout function here
-    return(x**2-3)
-x0=0
-x1=3
-#print(x0,x1)
-#rf.bisection(0,3)
-#rf.secant(0,3)
-#rf.newton(1.5)
+import pb1
+import matplotlib.pyplot as plt
+def g():        #the function for pseudo isothermal
+    def y(x):
+        return(((1+x**2)**(-0.5))-0.5)
+    return(y)
+
+def dg():      #the derivate of the function
+    def y(x):
+        return((-x)/((1+x**2)**(1.5)))
+    return(y)
+t=[0.001,0.0001,0.00001,0.000001,0.0000001,0.00000001]    #giving it threshold values
+#these will store number of iterations for each method with b being for bisection, s for secant, and n for newton
+b=[]
+s=[]
+n=[]
+for i in range(len(t)):
+    xb,bb=pb1.bisection(0,3,g(),t[i])
+    b.append(bb)
+    xs,bs=pb1.secant(0,3,g(),t[i])
+    s.append(bs)
+    xn,bn=pb1.newton(3,g(),dg(),t[i])
+    n.append(bn)
+plt.xlabel("Threshold values")
+plt.ylabel("Number of iterations")
+plt.title("Variation in number of iterations as the threshold is changed")
+plt.plot(t,b,marker="o",label="Bisection")
+plt.plot(t,s,marker="o",label="Secant")
+plt.plot(t,n,marker="o",label="Newton's")
+plt.legend()
+plt.savefig("HW1_pb2")
+print(b)
+print(s)
+print(n)
+    
+print(pb1.bisection(0,3,g()))
+print(pb1.secant(0,3,g()))
+print(pb1.newton(3,g(),dg()))
