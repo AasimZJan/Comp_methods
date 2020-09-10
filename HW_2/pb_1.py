5a162767b5762ff06b120a5fe574721469da523e
AasimZahoor <zahoorassim@gmail.com>
2020-09-09 23:45:42 -0400

    added comments

--- HW_2/pb_1.py
+++ HW_2/pb_1.py
@@ -7,38 +7,52 @@ Created on Wed Sep  9 15:13:08 2020
 """
 
 #%%
-import math
-def midpoint(f,a,b,n):
-    h=float(b-a)/n
-    x0=a
+
+#importing libraries
+import math                                  #for test cosine and sine function
+####
+####
+def midpoint(f,a,b,n):                       #arguments(f=the function, a=the starting point,
+                                             #b=the ending point,n the number of divisions)               
+    h=float(b-a)/n                           #width
+    x0=a                                     #initialising
     x1=x0+h
-    first=f((x0+x1)*0.5)*h
-    for i in range(n-1):
-        x0=x1
-        x1=x0+h
-        first=first+f((x0+x1)*0.5)*h
+    first=f((x0+x1)*0.5)*h                   #area of first rectangle
+    for i in range(n-1):                     #area of subsequent rectangles
+        x0=x1 
+        x1=x0+h 
+        first=first+f((x0+x1)*0.5)*h         #used recursion
     print('The value from midpoint rule is', first)
-def g():
+####    
+####
+#test function
+def g():                                        
     def y(x):
         return(math.cos(x))
     return(y)
-def trapezoid(f,a,b,n):
-    h=float(b-a)/n
+####
+####
+def trapezoid(f,a,b,n):                      #arguments(f=the function, a=the starting point,
+                                             #b=the ending point,n the number of divisions)  
+    h=float(b-a)/n                           
     x0=a
-    first=(f(x0))*h*0.5
-    for i in range(n-1):#n+1 points in summation
+    first=(f(x0))*h*0.5                      #area of first rectangle
+    for i in range(n-1):                     #area of subsequent rectangles
         x1=x0+h
-        first=first+(f(x1))*h
+        first=first+(f(x1))*h                #used recursion
         x0=x1
-    end=first+f(b)*h*0.5
+    end=first+f(b)*h*0.5                     #in the formula the last term had no coefficient so I       
     print('The value from trapezoid rule is', end)
-
-def simpson(f,a,b,n):
+####
+####
+def simpson(f,a,b,n):                        #arguments(f=the function, a=the starting point,
+                                             #b=the ending point,n the number of divisions)
     h=float(b-a)/n
     x0=a
-    first=f(x0)*h*(float(1)/3)
-    for i in range(n-1):
-        if (i+1)%2==0:
+    first=f(x0)*h*(float(1)/3)               #area of first rectangle
+    for i in range(n-1):                     #area of subsequent rectangles except last
+        if (i+1)%2==0:                       #f(x2),f(x4),f(x6)...terms had 2 as their coefficient and f(x1),f(x3),f(x5)....terms
+                                             # had 4 as their coefficient so I made if statements for that
             x1=x0+h
             first=first+(2*f(x1)*h*float(1)/3)
             x0=x1
@@ -46,11 +60,12 @@ def simpson(f,a,b,n):
             x1=x0+h
             first=first+(4*f(x1)*h*float(1)/3)
             x0=x1
-    end=first+f(b)*h*(1/3)
+    end=first+f(b)*h*(1/3)                  #area of last rectangle added to total
     print('The value from simpson rule is', end)
-
-def diff(f,a,h):
-    d=(f(a+h)-f(a))/h
+####
+####
+def diff(f,a,h=0.000001):                   #arguments(f=the function, a=the point of evaluation,h=step size(smaller the better)(careful to not make it too small))
+    d=(f(a+h)-f(a))/h                       #basic definition
 #    print(a,f(a))
     print('The result of differentiation is',d)
     
